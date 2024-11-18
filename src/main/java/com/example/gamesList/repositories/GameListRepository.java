0c4b89bf3921c292e963916e973399347fb43ba5
Xamacardoso <xcardoso1997@gmail.com>
2024-11-17 23:29:12 -0300

    feat: moving games in a list

--- src/main/java/com/example/gamesList/repositories/GameListRepository.java
+++ src/main/java/com/example/gamesList/repositories/GameListRepository.java
@@ -2,6 +2,12 @@ package com.example.gamesList.repositories;
 
 import com.example.gamesList.entities.GameList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
+    @Modifying // Used when the query it's not a selection query
+    @Query(nativeQuery = true, value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+    void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
+
 }
--- src/main/java/com/example/gamesList/services/GameListService.java
+++ src/main/java/com/example/gamesList/services/GameListService.java
@@ -2,7 +2,9 @@ package com.example.gamesList.services;
 
 import com.example.gamesList.dto.GameListDTO;
 import com.example.gamesList.entities.GameList;
+import com.example.gamesList.projections.GameMinProjection;
 import com.example.gamesList.repositories.GameListRepository;
+import com.example.gamesList.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,10 +17,26 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(x -> new GameListDTO(x)).toList();
     }
 
+    // Moves a game in a list locally, receiving an origin and a destination positional index
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex){
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+        GameMinProjection obj = list.remove(sourceIndex); // Removes the object from its source
+        list.add(destinationIndex, obj); // Inserts the object in the required position
+
+        int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+        int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+        for (int i=min; i<=max; i++){
+            gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
+    }
 }
